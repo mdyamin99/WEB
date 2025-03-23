06640119b6160c93dbfda78f125691f5c975fc5c
mdyamin99 <you@example.com>
2025-03-23 18:58:17 +0600

    Module-1 codes

---
+++ Python_course/Module-1/conditionals.py
@@ -0,0 +1,29 @@
+# in, not, not in, is, is not
+
+a=2
+if a>5:
+    print("A is largest")
+elif a>3:
+    print("olpo boro")
+elif a==2:
+    print("2 er soman")
+else:
+    print("A is smalest")
+
+boss=True
+
+if boss is True:
+    print("Tel er bakso niye aso boss re tel dibo")
+else:
+    print("Lunch er pore aso")
+
+coin = "tail"
+
+if boss==True:
+    print("Boss true")
+    if coin=="tail":
+        print("Bating")
+    else:
+        print("Bowling")
+else:
+    print("Boss is false")
---
+++ Python_course/Module-1/while.py
@@ -0,0 +1,20 @@
+# num=1
+# while num <= 10:
+#     print(num)
+#     num+=1
+
+# break
+# num=1
+# while num <= 10:
+#     print(num)
+#     if num==5:
+#         break
+#     num+=1
+
+# continue
+num=0
+while num <= 10:
+    num+=1
+    if num%2==1:
+        continue
+    print(num)
